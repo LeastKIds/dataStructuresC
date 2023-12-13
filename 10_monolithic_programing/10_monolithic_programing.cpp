ed89e86b1c3b6d6e46ef3ad65389157492c91090
leastkids <leastkids@gmail.com>
2023-12-13 06:33:59 +0000

    ADD - monolithic programing

---
+++ 10_monolithic_programing/10_monolithic_programing.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+
+
+int main() {
+
+    int length = 0, breadth = 0;
+
+    printf("Enter Length and Breadth");
+    cin>>length>>breadth;
+
+    int area = length * breadth;
+    int peri = 2 * (length + breadth);
+
+    printf("Aread = %d \nPerimeter = %d \n", area, peri);
+    
+
+    return 0;
+}
